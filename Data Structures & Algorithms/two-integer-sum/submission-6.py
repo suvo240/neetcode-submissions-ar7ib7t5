1e96bed27beb8961690f735af7342f8db6fac2a1
suvo240 <suvogaine786@gmail.com>
2026-05-07 10:26:37 +0530

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        resultDict = {}
+
+        finalRes = []
+
+        for i in range(len(nums)):
+            if (target - nums[i]) in resultDict:
+                finalRes.append(resultDict.get(target - nums[i]))
+                finalRes.append(i)
+            resultDict[nums[i]]=i
+        #finalRes.sort()
+        return finalRes
+        
